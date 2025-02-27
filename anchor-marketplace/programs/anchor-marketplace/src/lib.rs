5eb97899b4f94368b7f6be8c727d0ed94b5b73a5
vitorribeiro99 <73917262+vitorribeiro99@users.noreply.github.com>
2025-02-27 18:15:48 +0000

    feat: anchor marketplace (normal nft)

---
+++ anchor-marketplace/programs/anchor-marketplace/src/lib.rs
@@ -0,0 +1,43 @@
+use anchor_lang::prelude::*;
+
+declare_id!("32hxKfwxEcVqSoGWGYeRKSSkjJN5UUPZgKMAqvngkvxo");
+
+mod state;
+mod errors;
+
+mod contexts;
+use contexts::*;
+use errors::*;
+
+#[program]
+pub mod anchor_marketplace {
+
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>, name: String, fee: u16) -> Result<()> {
+        ctx.accounts.init(name, fee, &ctx.bumps)?;
+
+        Ok(())
+    }
+
+    pub fn listing(ctx: Context<List>, price: u64) -> Result<()> {
+        ctx.accounts.create_listing(price, &ctx.bumps)?;
+        ctx.accounts.deposit_nft()?;
+
+        Ok(())
+    }
+
+    pub fn delist(ctx: Context<Delist>) -> Result<()> {
+        ctx.accounts.withdraw_nft()?;
+
+        Ok(())
+    }
+
+    pub fn purchase(ctx: Context<Purchase>) -> Result<()> {
+        ctx.accounts.send_sol()?;
+        ctx.accounts.send_nft()?;
+        ctx.accounts.close_mint_vault()?;
+
+        Ok(())
+    }
+}
